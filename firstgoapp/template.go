8ada6b7f97ba78b8eb92ce10b4bc48ed22f8f264
Ubuntu <ubuntu@ip-172-31-17-15.us-east-2.compute.internal>
2019-11-12 14:13:40 +0000

    first update in the feature1 branch

---
+++ firstgoapp/template.go
@@ -0,0 +1,49 @@
+package main
+
+import(
+   "html/template"
+   "log"
+   "net/http"
+   "time"
+)
+
+type PageVariables struct {
+
+       Date    string
+       Time    string
+
+
+}
+
+func main(){
+
+         http.HandleFunc("/", HomePage)
+         log.Fatal(http.ListenAndServe(":8080", nil))   
+
+
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request){
+
+       now := time.Now()
+       HomePageVars := PageVariables{
+         Date: now.Format("02-01-2006"), 
+         Time: now.Format("15:04:05"),   
+
+}
+
+       t, err := template.ParseFiles("homepage.html")
+       if err != nil{
+              log.Print("tmeplate parsing error: ", err)
+              
+}   
+
+      err = t.Execute(w, HomePageVars)
+      if err !=nil {
+               log.print("template executing error: ", err)
+
+ } 
+
+
+
+}
